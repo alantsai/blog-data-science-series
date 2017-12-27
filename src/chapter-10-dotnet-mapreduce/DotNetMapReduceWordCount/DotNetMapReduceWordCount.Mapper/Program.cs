81c78a8362a773e3fc99acc6d384e2e459a5c2e4
Alan Tsai <alan@alantsai.net>
2017-12-27 22:48:33 +0800

    add Mapper

---
+++ src/chapter-10-dotnet-mapreduce/DotNetMapReduceWordCount/DotNetMapReduceWordCount.Mapper/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace DotNetMapReduceWordCount.Mapper
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string line;
+
+            while ((line = Console.ReadLine()) != null)
+            {
+                var words = Regex.Matches(line, @"[\w]+");
+
+                foreach (var word in words)
+                {
+                    Console.WriteLine("{0}\t1", word);
+                }
+            }
+        }
+    }
+}
